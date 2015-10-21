f2ce6685ba07d85a4c1b9bf08953eaefdf50ad48
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-10-21 12:55:31 +0200

    correct line height for big screens

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -3,5 +3,7 @@ import Component from 'lib/components/Component';
 export default class Note extends Component {
   constructor($el) {
     super($el);
+
+    this.$point = this.$el.querySelector('.Note-point');
   }
 }
