5e9694efabbee6e376b440cffb9b64a70a87012e
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-10-21 20:50:31 +0200

    rollover for reversed titles

--- src/components/project-title/ProjectTitle.js
+++ src/components/project-title/ProjectTitle.js
@@ -1,9 +1,11 @@
 import Component from 'lib/components/Component';
+import classes from 'dom-classes';
 
 export default class ProjectTitle extends Component {
   constructor($el) {
     super($el);
 
+    this.isReversed = classes.has(this.$el, 'reverse');
     this.isVisible = false;
     this.$line = this.$el.querySelector('.ProjectTitle-line');
     this.$separator = this.$el.querySelector('.ProjectTitle-separator');
@@ -39,16 +41,22 @@ export default class ProjectTitle extends Component {
   }
 
   setTimelines() {
+    let dir = 1;
+
+    if (this.isReversed) {
+      dir = -1;
+    }
+
     this.tlIn.fromTo(this.$line, 0.6, { scaleY: 0 }, { scaleY: 1, ease: Cubic.easeOut }, 0.25);
     this.tlIn.fromTo(this.$separator, 0.5, { scaleY: 0 }, { scaleY: 1, ease: Cubic.easeOut }, 0.55);
-    this.tlIn.fromTo(this.$author, 1.2, { x: -290 }, { x: 2, ease: Cubic.easeOut }, 0.5);
-    this.tlIn.fromTo(this.$number, 0.6, { x: 45 }, { x: 0, ease: Cubic.easeOut }, 0.75);
-    this.tlIn.fromTo(this.$title, 0.8, { x: -150 }, { x: 2, ease: Cubic.easeOut }, 0.85);
+    this.tlIn.fromTo(this.$author, 1.2, { x: -290 * dir }, { x: 2, ease: Cubic.easeOut }, 0.5);
+    this.tlIn.fromTo(this.$number, 0.6, { x: 45 * dir }, { x: 0, ease: Cubic.easeOut }, 0.75);
+    this.tlIn.fromTo(this.$title, 0.8, { x: -150 * dir }, { x: 2, ease: Cubic.easeOut }, 0.85);
     this.tlIn.pause(0);
 
-    this.tlOut.to(this.$author, 1.0, { x: -290, ease: Cubic.easeOut }, 0);
-    this.tlOut.to(this.$number, 0.4, { x: 45, ease: Cubic.easeOut }, 0.1);
-    this.tlOut.to(this.$title, 0.6, { x: -150, ease: Cubic.easeOut }, 0.2);
+    this.tlOut.to(this.$author, 1.0, { x: -290 * dir, ease: Cubic.easeOut }, 0);
+    this.tlOut.to(this.$number, 0.4, { x: 45 * dir, ease: Cubic.easeOut }, 0.1);
+    this.tlOut.to(this.$title, 0.6, { x: -150 * dir, ease: Cubic.easeOut }, 0.2);
     this.tlOut.to(this.$separator, 0.5, { scaleY: 0, ease: Cubic.easeOut }, 0.3);
     this.tlOut.to(this.$line, 0.5, { scaleY: 0, ease: Cubic.easeOut }, 0.4);
     this.tlOut.pause(0);
