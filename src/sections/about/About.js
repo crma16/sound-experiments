2ac4d6690b103898a0ef1dd13a03592c3e385677
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-10-22 23:41:01 +0200

    about

--- src/sections/about/About.js
+++ src/sections/about/About.js
@@ -4,6 +4,10 @@ import Mediator from 'lib/Mediator';
 export default class About extends Section {
   constructor($el) {
     super($el);
+
+    this.tl = new TimelineMax();
+    this.tl.staggerFromTo(this.$el.querySelectorAll('p'), 1.2, { y: 100, alpha: 0 }, { y: 0, alpha: 1, ease: Expo.easeOut }, 0.08, 0);
+    this.tl.pause(0);
   }
 
   onInit() {}
@@ -12,5 +16,6 @@ export default class About extends Section {
     super.transitionIn(callback);
     Mediator.emit('header:transitionIn');
     Mediator.emit('footer:transitionIn');
+    this.tl.play(0);
   }
 }
